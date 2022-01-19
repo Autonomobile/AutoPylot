99dd86c5065720a3a7f1d1c36cab38bbf02bd755
Alexandre <killersgg78@gmail.com>
2022-01-20 00:32:28 +0100

    def load_image

--- AutoPylot/utils/IO.py
+++ AutoPylot/utils/IO.py
@@ -1,2 +1,13 @@
 """ Here we will load and save data""" 
-#lol gros lol, salut sale merde Maxime va manger. Papa est pas content !
+from PIL import Image 
+from numpy import asarray
+#from os import listdir
+#not need for list of images here.
+
+def load_image(path): #Should return the image as array.
+    image = PImage.open(path)
+    array_data = asarray(image)
+    return array_data
+
+
+print (load_images("C:\\Users\\sacha\\Pictures\\Saved Pictures\\1")) #testing if wokring
