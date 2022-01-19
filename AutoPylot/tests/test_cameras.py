56c531dee8fb725a0dcaa8669cb9e9605e7fa244
Maxime Ellerbach <maxime@ellerbach.net>
2022-01-19 11:51:15 +0100

    uploaded some tests !

--- AutoPylot/tests/test_cameras.py
+++ AutoPylot/tests/test_cameras.py
@@ -1,6 +1,17 @@
 import unittest
+import context
+from cameras import camera
 
-from . import context
-import cameras
 
-# TODO
+def test_dummy_capture():
+    shape = (160, 120, 3)
+    cam = camera.Camera(camera_type="dummy", shape=shape)
+    img = cam.read()
+    assert img.shape == shape
+
+
+def test_dummy_color():
+    shape = (160, 120, 1)
+    cam = camera.Camera(camera_type="dummy", shape=shape)
+    img = cam.read()
+    assert img.shape == shape
