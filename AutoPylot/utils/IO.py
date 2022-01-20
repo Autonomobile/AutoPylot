9a7975a8a079aa585ea1592aebcc3dd4aad4b385
Alexandre <killersgg78@gmail.com>
2022-01-20 09:17:27 +0100

    updated IO.py

--- AutoPylot/utils/IO.py
+++ AutoPylot/utils/IO.py
@@ -1,6 +1,8 @@
 """ Here we will load and save data""" 
 from PIL import Image 
 from numpy import asarray
+import json
+import cv2
 
 def load_image(path): #Should return the image as array.
     image = PImage.open(path)
@@ -8,12 +10,18 @@ def load_image(path): #Should return the image as array.
     return array_data
 
 
-print (load_images("C:\\Users\\sacha\\Pictures\\Saved Pictures\\1")) #testing if wokring
+def save_image(path,new_path): #Save the image from the normal image
+    image = PImage.open(path)
+    image.save(new_path)
 
-def save_image(path):
+def save_image(path,new_path): #Probably the right version (using the numpy.array data)
+    image=Image.fromarray(save_image(path))
+    image.save(new_path)
 
 
-def laod_json(path):
+def laod_json(path):#path is the path o the json file
+    json = json.loads(path)
+    
 
 
 def save_json(path):
