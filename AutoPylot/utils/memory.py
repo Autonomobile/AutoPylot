b84fc600880ace5e064cf2b6e00de6773bd84a22
Maxime Ellerbach <maxime@ellerbach.net>
2022-01-20 22:42:00 +0100

    Memory (#15)
    * added memory
    
    * small change
    
    * small change (trying to see if it triggers the test)

---
+++ AutoPylot/utils/memory.py
@@ -0,0 +1,31 @@
+"""Memory class that we will be using throughout the project."""
+import time
+
+
+class Memory(dict):
+    """Memory class, inherits from dict."""
+
+    def __init__(self):
+        """Init of the memory class."""
+        self.last_modified = 0
+
+    def __setitem__(self, key, value):
+        """Set a key to a value.
+
+        Args:
+            key (string or integer): key.
+            value (any): value.
+        """
+        self.last_modified = time.time()
+        super().__setitem__(key, value)
+
+    def __add__(self, other):
+        """Addition of two dict.
+
+        Args:
+            other (dict): dict to add to.
+        """
+        if isinstance(other, dict):
+            super().update(other)
+        else:
+            raise TypeError("other must be a dict")
