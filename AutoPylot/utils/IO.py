71280c99bee0ebce85f9abb30b236b9ddb89b15a
Alexandre <killersgg78@gmail.com>
2022-01-21 00:42:46 +0100

    Load image working

--- AutoPylot/utils/IO.py
+++ AutoPylot/utils/IO.py
@@ -1,8 +1,15 @@
 """ Here we will load and save data comming from the images""" 
+from tkinter import CASCADE
 from numpy import asarray
 import json 
 import cv2
 import os
+
+"""When on windows don't copy the path directly use one of the following ways: 
+1.  use an r (raw) before the copied path (r "C:\...\...")
+2.  use the normal /
+3.  use the \\ ("C:\\...\\...")
+"""
 
 
 def load_image(path): #Should return the image as array (num_rows, num_cols, num_channels).
@@ -13,15 +20,13 @@ def save_image(path,save_path): #Probably the right version (using the numpy.arr
     return cv2.imwrite(save_path,load_image(path))
 
 
-def laod_json(path):#path is the path o the json file
+def laod_json(path):#path is the path o the json file.
     return json.load(path)
-    data.close()
     
 
 def save_json(path):
     return json.dump(laod_json(path))
 
+print (load_image("C:\\Users\\sacha\\Downloads\\kid.jpg"))
 
-def load_both (json_path):
-    
-
+#def load_both (json_path):
