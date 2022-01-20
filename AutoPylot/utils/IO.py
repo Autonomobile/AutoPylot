ec45093b48308ed05470005c49fd16fb3376b7e4
Alexandre <killersgg78@gmail.com>
2022-01-20 16:10:47 +0100

    deleted wrong version saveimage

--- AutoPylot/utils/IO.py
+++ AutoPylot/utils/IO.py
@@ -1,7 +1,7 @@
-""" Here we will load and save data""" 
+""" Here we will load and save data comming from the images""" 
 from PIL import Image 
 from numpy import asarray
-import json
+import json 
 import cv2
 
 def load_image(path): #Should return the image as array.
@@ -9,10 +9,6 @@ def load_image(path): #Should return the image as array.
     array_data = asarray(image)
     return array_data
 
-
-def save_image(path,new_path): #Save the image from the normal image
-    image = PImage.open(path)
-    image.save(new_path)
 
 def save_image(path,new_path): #Probably the right version (using the numpy.array data)
     image=Image.fromarray(save_image(path))
