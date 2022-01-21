f49498b078a20dae6e50cc6fe12b7886a38199ef
Alexandre <killersgg78@gmail.com>
2022-01-21 22:36:54 +0100

    convention pour max et moi

--- autopylot/utils/io.py
+++ autopylot/utils/io.py
@@ -3,20 +3,20 @@ from numpy import asarray
 import json 
 import cv2
 
-
-def load_image(path): #Should return the image as array (num_rows, num_cols, num_channels).
+#Should return the image as array (num_rows, num_cols, num_channels).
+def load_image(path): 
     return cv2.imread(path)
 
+ #saves an image using the numpy.array data
+def save_image(path, save_path):
+    return cv2.imwrite(save_path, load_image(path))
 
-def save_image(path,save_path): #Probably the right version (using the numpy.array data)
-    return cv2.imwrite(save_path,load_image(path))
-
-
-def laod_json(path):#path is the path to the json file.
+#path is the path to the json file.
+def laod_json(path):
     return json.load(path)
     
-
-def save_json(path):#saving  the json file.
+#saving the json file. (not working)
+def save_json(path):
     return json.dump(laod_json(path))
 
 
