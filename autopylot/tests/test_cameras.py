886380c4005c5157a35a53f58a80838f79e1c7d7
Maxime Ellerbach <maxime@ellerbach.net>
2022-01-21 12:09:38 +0100

    added serial control

--- autopylot/tests/test_cameras.py
+++ autopylot/tests/test_cameras.py
@@ -7,7 +7,7 @@ def test_dummy_capture():
     shape = (160, 120, 3)
     test_memory = {}
     cam = camera.Camera(test_memory, camera_type="dummy", shape=shape)
-    cam.read()
+    cam.update()
     assert test_memory['image'].shape == shape
 
 
@@ -15,5 +15,5 @@ def test_dummy_color():
     shape = (160, 120, 1)
     test_memory = {}
     cam = camera.Camera(test_memory, camera_type="dummy", shape=shape)
-    cam.read()
+    cam.update()
     assert test_memory['image'].shape == shape
