39ff307b03d0457e8f2363e39aee5ab0c7fd9852
Alexandre <killersgg78@gmail.com>
2022-01-21 22:04:43 +0100

    test_save_image

--- autopylot/tests/test_io.py
+++ autopylot/tests/test_io.py
@@ -1,20 +1,25 @@
 from ..utils import io
 
-"""When on windows don't copy the path directly use one of the following ways: 
+"""
+When on windows don't copy the path directly use one of the following ways: 
 1.  use an r (raw) before the copied path (r "C:\...\...")
 2.  use the normal / ("C:/.../..")
 3.  use the \\ ("C:\\...\\...")
+
+The images should be with .jpg or .png extension.
+The Json file should be with .json extension.
 """
 
 
 def test_load_image():
-    image = io.load_image('/.../.../image.jpg')
-    assert image, "should not be None"
+    image = io.load_image('../.../image.jpg')
+    assert image, "should not be None, given path no correct."
 
 
 def test_save_image():
-
-    assert
+    image = io.load_image('../.../image.jpg')
+    io.save_image(image, '../.../saved_image.jpg')
+    assert io.save_image(image, '../.../saved_image.jpg'), "should not be None."
 
 def test_laod_json():
 
