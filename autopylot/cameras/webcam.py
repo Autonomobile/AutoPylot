5e6035a131b90f5c907429eb40ab54d7de22a305
Maxime Ellerbach <maxime@ellerbach.net>
2022-01-24 15:15:24 +0100

    Serial control and controller (#23)
    * added serial control and controller
    
    * added pyserial in requirements.txt
    
    * added a bunch of tests

--- autopylot/cameras/webcam.py
+++ autopylot/cameras/webcam.py
@@ -19,14 +19,14 @@ class Webcam:
         assert ret, "Couldn't read from camera"
 
         self.shape = shape
-        assert len(self.shape) == 3, "Shape should have 3 dimensions"
         self.h = shape[0]
         self.w = shape[1]
         self.c = shape[2]
+        assert len(self.shape) == 3, "Shape should have 3 dimensions"
         assert self.c in [
             1, 3], "Image last dimension should be either 3 (RGB) or 1 (GREY)"
 
-    def read(self):
+    def update(self):
         """Read image from the camera.
 
         Raises:
--- autopylot/tests/test_cameras.py
+++ autopylot/tests/test_cameras.py
@@ -1,13 +1,12 @@
 """Test the cameras."""
-import context
-from cameras import camera
+from ..cameras import camera
 
 
 def test_dummy_capture():
     shape = (160, 120, 3)
     test_memory = {}
     cam = camera.Camera(test_memory, camera_type="dummy", shape=shape)
-    cam.read()
+    cam.update()
     assert test_memory['image'].shape == shape
 
 
@@ -15,5 +14,5 @@ def test_dummy_color():
     shape = (160, 120, 1)
     test_memory = {}
     cam = camera.Camera(test_memory, camera_type="dummy", shape=shape)
-    cam.read()
+    cam.update()
     assert test_memory['image'].shape == shape
