7c4917c5ffae0cdb0dcd3f91ae79e8db67c6171c
Maxime Gay <maxime.gay@epita.fr>
2022-01-25 22:46:29 +0100

    load_image_data

--- autopylot/utils/io.py
+++ autopylot/utils/io.py
@@ -28,22 +28,54 @@ def save_image(path, image):
     """
     return cv2.imwrite(path, image)
 
-# path is the path to the json file.
+
 def load_json(path):
+    """Load a json file
+
+    Args:
+        path ([string]): path of the json file to load
+
+    Returns:
+        [dictionary]: data about the image 
+    """
     with open(os.path.normpath(path), "r") as json_file:
         return json.load(json_file)
 
-# saving the json file.
-
-def load_both (json_path): #load image associate to json file
-    json_data = json.load(json_path) #path of the json file
-    image_path = cv2.imread(json_path) #deduce the path of the image 
-    image = cv2.imread(image_path) #load image .png
-    return image, json_data
 
 def save_json(path, to_save):
+    """Save the Json file
+
+    Args:
+        path ([string]): path of the json file
+        to_save ([string]): path of where we have to save this file
+
+    Returns:
+        [dictionary]: save data about the image at the path "to_save"
+    """
     with open(os.path.normpath(path), "w") as json_file:
         return json.dump(to_save, path)
 
 
-#def load_both (json_path):
+def load_image_data (json_path):
+    """load a json file and an image 
+
+    Args:
+        json_path ([string]): path of the json file 
+
+    Returns:
+        [tuple]: image and json file
+    """
+    tmp = json_path.split(".")
+    tmp.pop()
+    tmp.append(".png")
+    image_path = ""
+    for i in tmp :
+        image_path += i
+
+    with open(os.path.normpath(json_path), "r") as json_file:
+        data = {
+            "image" : cv2.imread(image_path),
+            "json_file" :json.load(json_path)
+        } 
+    return data
+    
