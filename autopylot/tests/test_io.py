1d10ba44194b790dd5a8838d1bd19272738e59c5
Alexandre <killersgg78@gmail.com>
2022-01-27 14:19:30 +0100

    not finished

--- autopylot/tests/test_io.py
+++ autopylot/tests/test_io.py
@@ -1,6 +1,6 @@
 """test the io class"""
-
-from ..utils import io
+import os
+#from ..utils import io
 
 """
 When on windows don't copy the path directly use one of the following ways: 
@@ -11,26 +11,36 @@ When on windows don't copy the path directly use one of the following ways:
 The images should be with .jpg or .png extension.
 The Json file should be with .json extension.
 """
+#create a test_io directory which will be deleted afterwards.
 
-#creer un dossier 
-def create_file (path):
-    pass
-    
+def create_directory():
+    current_path = os.getcwd()
+    new_path_dir = os.path.join(current_path,"testing_io")
+    os.mkdir(new_path_dir)
 
 
+
+def delete_directory():
+    path = os.getcwd()
+    path_dir = os.path.join(path,"testing_io")
+    os.remove(path_dir)
+    return "succesfully removed temp dir"
+
+delete_directory()
+"""
 def test_load_image():
     image = io.load_image("../.../image.jpg")
-    assert image is None, "should not be None, given path no correct."
+    assert image is None, "should not be None, given path no correct/not excistent."
 
 
 def test_save_image():
     image = io.load_image("../.../image.jpg")
-    io.save_image(image, "../.../saved_image.jpg")
-    assert io.save_image(image, '../.../saved_image.jpg') is True, "should not be None."
+    save = io.save_image(image, "../.../saved_image.jpg")
+    assert save is True, "should not be None."
 
 def test_laod_json():
     file = io.laod_json("../.../file.json")
-    assert file, "should not be None."
+    assert file is None, "should not be None."
 
 # def test_save_json():
 
@@ -39,3 +49,4 @@ def test_laod_json():
 # def test_both():
 
 #     assert
+"""
