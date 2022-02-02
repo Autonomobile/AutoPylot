8b26c84775456a872b762e9e15cadf946e6b63ae
Alexandre <killersgg78@gmail.com>
2022-02-02 23:50:39 +0100

    black working good

--- autopylot/datasets/FileIO.py
+++ autopylot/datasets/FileIO.py
@@ -1,8 +1,9 @@
 """ Here we will load and save dataset."""
-import os 
+import os
 import json
 import cv2
 from ..utils import io
+
 
 def load_dataset(dirpath):
     """load dataset not finish (I will delete the other comments in green).
@@ -13,16 +14,23 @@ def load_dataset(dirpath):
     Returns:
          dictionnary: tuple image an json.
     """
-    topdir = '.' # top argument : need more explaination
-    exten_json = '.json' # the extension for the search 
-    exten_png = '.png'
-    for dirpath, dirnames, files in os.walk(topdir): #os.walk returns 3 value, dirpath, dirnames and filenames
+    topdir = "."  # top argument : need more explaination
+    exten_json = ".json"  # the extension for the search
+    exten_png = ".png"
+    for dirpath, dirnames, files in os.walk(
+        topdir
+    ):  # os.walk returns 3 value, dirpath, dirnames and filenames
         for json_file_name in files:
             if json_file_name.lower().endswith(exten_json):
-                load_json(os.path.join(dirpath, json_file_name)) #should execute the load_json fn with the path of each json file
+                load_json(
+                    os.path.join(dirpath, json_file_name)
+                )  # should execute the load_json fn with the path of each json file
         for png_file_name in files:
             if png_file_name.lower().endswith(exten_png):
-                load_image(os.path.join(dirpath, png_file_name)) #should execute the load_json fn with the path of each json file 
-    
+                load_image(
+                    os.path.join(dirpath, png_file_name)
+                )  # should execute the load_json fn with the path of each json file
+
+
 def load_multiple_dataset(dirpaths):
     pass
