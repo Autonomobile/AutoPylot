7112602305311649ba6d1c95271028c8d4dd5efc
Maxime Ellerbach <maxime@ellerbach.net>
2022-02-02 20:31:21 +0100

    big code formatting using "black"

--- autopylot/tests/test_state_switcher.py
+++ autopylot/tests/test_state_switcher.py
@@ -9,7 +9,7 @@ def test_switcher_stop_empty():
     sw = state_switcher.StateSwitcher(mem)
     sw.update()
 
-    assert mem['state'] == "stop"
+    assert mem["state"] == "stop"
 
 
 def test_switcher_stop_disconnected():
@@ -22,48 +22,72 @@ def test_switcher_stop_disconnected():
     js.update()
     sw.update()
 
-    assert mem['state'] == "stop"
+    assert mem["state"] == "stop"
 
 
 def test_switcher_stop_button():
     """If the controller is connected and the button Y is pressed, then stop."""
-    mem = {'controller': {'steering': 0.2, 'throttle': 0.1,
-                          'button_y': True, 'button_a': False}}
+    mem = {
+        "controller": {
+            "steering": 0.2,
+            "throttle": 0.1,
+            "button_y": True,
+            "button_a": False,
+        }
+    }
 
     sw = state_switcher.StateSwitcher(mem)
     sw.update()
 
-    assert mem['state'] == "stop"
+    assert mem["state"] == "stop"
 
 
 def test_switcher_autonomous():
     """If the controller is connected and both the steering and throttle are 0.0, then autonomous."""
-    mem = {'controller': {'steering': 0.0, 'throttle': 0.0,
-                          'button_y': False, 'button_a': False}}
+    mem = {
+        "controller": {
+            "steering": 0.0,
+            "throttle": 0.0,
+            "button_y": False,
+            "button_a": False,
+        }
+    }
 
     sw = state_switcher.StateSwitcher(mem)
     sw.update()
 
-    assert mem['state'] == "autonomous"
+    assert mem["state"] == "autonomous"
 
 
 def test_switcher_collect():
     """If the controller is connected and the button A is pressed, then collect."""
-    mem = {'controller': {'steering': 0.0, 'throttle': 0.0,
-                          'button_y': False, 'button_a': True}}
+    mem = {
+        "controller": {
+            "steering": 0.0,
+            "throttle": 0.0,
+            "button_y": False,
+            "button_a": True,
+        }
+    }
 
     sw = state_switcher.StateSwitcher(mem)
     sw.update()
 
-    assert mem['state'] == "collect"
+    assert mem["state"] == "collect"
 
 
 def test_switcher_manual():
     """If the controller is connected and both button Y and A are not pressed, then manual (no collect)."""
-    mem = {'controller': {'steering': 0.2, 'throttle': 0.1,
-                          'button_y': False, 'button_a': False}}
+    mem = {
+        "controller": {
+            "steering": 0.2,
+            "throttle": 0.1,
+            "button_y": False,
+            "button_a": False,
+        }
+    }
 
     sw = state_switcher.StateSwitcher(mem)
     sw.update()
 
-    assert mem['state'] == "manual"
+    assert mem["state"] == "manual"
