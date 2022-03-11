bac2a9e9cb5177467d89c3ec05a36386b1d645bc
Mickael Bobovitchcd <m.bobovitch@gmail.com>
2022-03-11 02:00:16 +0100

    socket io client

---
+++ telemetry/utils/client.js
@@ -0,0 +1,23 @@
+import { atom } from "jotai";
+import io from "socket.io-client";
+
+export function getSocket() {
+  const socket = io();
+  socket.on("connect", () => {
+    socket.emit("ui-client-connected");
+  });
+  const onError = (error) => {
+    socket.disconnect();
+  };
+  socket.on("connect_failed", onError);
+  socket.on("connect_error", onError);
+  socket.on("connect_timeout", onError);
+  socket.on("error", onError);
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+    setSocket(io());
+  });
+  return socket;
+}
+
+export const socketAtom = atom(getSocket());
