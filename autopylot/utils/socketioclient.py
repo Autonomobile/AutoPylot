ebccb1b83497b871b268394db50c8f7d6996ef90
Maxime Ellerbach <maxime@ellerbach.net>
2022-03-12 00:01:20 +0100

    updated client

--- autopylot/utils/socketioclient.py
+++ autopylot/utils/socketioclient.py
@@ -1,5 +1,7 @@
-import socketio
+import time
 from collections import deque
+
+import socketio
 
 log_queue = deque(maxlen=100)
 telemetry_queue = deque(maxlen=5)
@@ -24,13 +26,38 @@ def on_message(data):
     print(data)
 
 
-def run_threaded(host):
-    sio.connect(host)
+def wait_for_connection(host, sleep=1):
+    while not sio.connected and not stop_thread:
+        try:
+            sio.connect(host)
+        except Exception:
+            time.sleep(sleep)
 
-    while True:
-        if len(log_queue) != 0:
-            log = log_queue.popleft()
-            sio.emit("logs", log)
-        if len(telemetry_queue) != 0:
-            telemetry = telemetry_queue.popleft()
-            sio.emit("telemetry", telemetry)
+
+def wait_for_reconnection(sleep=1):
+    while not sio.connected:
+        time.sleep(sleep)
+
+
+def send_log(log):
+    sio.emit("logs", log)
+
+
+def send_telemetry(telemetry):
+    sio.emit("telemetry", telemetry)
+
+
+def run_threaded(host):
+    wait_for_connection(host)
+
+    while not stop_thread:
+        if not sio.connected:
+            wait_for_connection(host)
+
+        for _ in range(len(log_queue)):
+            send_log(log_queue[0])
+            del log_queue[0]
+
+        for _ in range(len(telemetry_queue)):
+            send_telemetry(telemetry_queue[0])
+            del telemetry_queue[0]
