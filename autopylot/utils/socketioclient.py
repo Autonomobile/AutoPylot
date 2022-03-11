e841562160bb5038dcafe480fe2d8b37cd3b1084
Maxime Ellerbach <maxime@ellerbach.net>
2022-03-12 00:11:39 +0100

    added uid

--- autopylot/utils/socketioclient.py
+++ autopylot/utils/socketioclient.py
@@ -1,5 +1,6 @@
 import time
 from collections import deque
+import uuid
 
 import socketio
 
@@ -8,12 +9,13 @@ telemetry_queue = deque(maxlen=5)
 stop_thread = False
 
 sio = socketio.Client()
+uid = uuid.uuid4()
 
 
 @sio.on("connect")
 def on_connect():
     print("connected")
-    sio.emit("py-client-connected")
+    sio.emit("py-client-connected", uid)
 
 
 @sio.on("disconnect")
