af27a65f90834dcfb8292d036fffb4c20cc5f709
Maxime Ellerbach <maxime@ellerbach.net>
2022-05-08 13:14:04 +0200

    experiencing some stuff

--- autopylot/models/architectures.py
+++ autopylot/models/architectures.py
@@ -213,7 +213,7 @@ class Models:
         logging.info(f"created gigachad model with {get_flops(model)} FLOPS")
         return model
 
-    def synthetic_data_model():
+    def synthetic_model():
         """Basically a clone of gigachad model, but without the zone output."""
         inputs = []
         outputs = []
@@ -222,28 +222,36 @@ class Models:
         inputs.append(inp)
 
         x = Cropping2D(cropping=((20, 20), (0, 0)))(inp)
-        x = BatchNormalization()(x)
+        x = Lambda(lambda x: x / 127.5 - 1)(x)
 
         x = Conv2D(12, 5, strides=2, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Conv2D(24, 5, strides=2, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Conv2D(32, 5, strides=2, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Conv2D(48, 3, strides=2, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Conv2D(64, 3, strides=1, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
 
         x = Flatten()(x)
         x = Dropout(0.2)(x)
 
         x = Dense(200, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Dense(100, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Dense(100, use_bias=False)(x)
         x = Activation("relu")(x)
+        x = BatchNormalization()(x)
         x = Dropout(0.1)(x)
 
         y1 = Dense(1, use_bias=False, activation="tanh", name="steering")(x)
