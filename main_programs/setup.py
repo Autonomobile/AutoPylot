5713e99ca171e10778b15af0beff0ccd03790e72
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-06-06 21:34:24 +0200

    added installation helper

---
+++ main_programs/setup.py
@@ -0,0 +1,13 @@
+'''
+Before working with autopylot, which include training and predicting,
+you need to setup the environment first. To do so, just run the following
+command:
+
+    python setup.py
+
+!!! Make sure autopylot is installed !!!
+
+it will generate a settings.json file in autopylot's root directory and
+it will create $HOME/dataset and $HOME/collect directories.
+'''
+from autopylot.utils import settings
